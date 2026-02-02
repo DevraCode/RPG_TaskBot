c6d7f29f15bce435f85ea0ed2a65819fd49e24bc
Ibuetab <ibuetab@gmail.com>
2026-02-02 22:52:41 +0100

    Arreglados algunos fallos

--- functions/characters_functions.py
+++ functions/characters_functions.py
@@ -1,5 +1,9 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputMedia
 from telegram.ext import CallbackContext, ConversationHandler
+
+from telegram.constants import ParseMode
 
 #---------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------
@@ -8,8 +12,7 @@ from functions.basic_functions import generate_id
 from functions.characters_data import male_warrior_01, female_warrior_01, male_mage_01, female_mage_01
 #from functions.characters_data import male_warrior, female_warrior, male_mage, female_mage
 
-
-
+character_list = [male_warrior_01,female_warrior_01,male_mage_01,female_mage_01]
 
 
 #Función que SOLO MUESTRA la galeria de personajes
@@ -26,35 +29,25 @@ async def show_characters(update:Update,context:CallbackContext):
     #Botones de Anterior, Siguiente y Seleccionar
     else:
 
-        
-        buttons = [
-            InlineKeyboardButton("Previous", callback_data="PREVIOUS"),
-            InlineKeyboardButton("Next", callback_data="NEXT")
+        index = 0
+        keyboard = [
+            [
+                InlineKeyboardButton("Previous", callback_data=f"PREV_{index}"),
+                InlineKeyboardButton("Next", callback_data=f"NEXT_{index}")
+            ],
+            [InlineKeyboardButton("Select", callback_data=f"SELECT_{index}")]
         ]
-
-        select_character = [InlineKeyboardButton("Select", callback_data="SELECT")]
-
-
-        keyboard.append(buttons)
-        keyboard.append(select_character)
+    
         reply_markup = InlineKeyboardMarkup(keyboard)
-
+        
 
         #Se muestra el primer personaje de la lista, y con los botones pasaremos a los demás
-        await context.bot.send_sticker(chat_id = chat_id, sticker = male_warrior_01)
-        await update.message.reply_text(text=f"Guerrero",reply_markup=reply_markup)
+        await context.bot.send_sticker(
+        chat_id=chat_id, 
+        sticker=character_list[index],
+        reply_markup=reply_markup
+    )
+
+        
 
 
-async def show_characters_buttons(update:Update,context:CallbackContext):
-    chat_id = update.effective_chat.id
-    user_id = generate_id(chat_id)
-
-    query = update.callback_query
-    await query.answer()
-
-    data = query.data
-
-    character_list = [male_warrior_01,female_warrior_01,male_mage_01,female_mage_01]
-
-    
-    
